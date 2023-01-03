f6e99a64c2eeea6ac166b65a5f99c944765a5d21
op46s <operator46@icloud.com>
2023-01-03 20:44:18 +0000

    .

---
+++ JS Basics/First Steps in Coding-Lab/8.PetShop.js
@@ -0,0 +1,10 @@
+function zooShop (input) {
+
+    let dogFoodTotal = (Number(2,50) * input [0]);
+    let catFoodTotal = (Number(4) * input[1]);
+    console.log ("");
+}
+
+zooShop ([5], [4]);
+
+//	"{крайната сума} lv."
