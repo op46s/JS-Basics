1e0e3d72dddd87c47e27aadb9b20c44a71e77c25
op46s <operator46@icloud.com>
2023-01-04 09:31:14 +0000

    09:31

--- JS Basics/First Steps in Coding-Exercise/01.USDtoBGN.js
+++ JS Basics/First Steps in Coding-Exercise/01.USDtoBGN.js
@@ -1,4 +1,9 @@
-function usdtobgn () {
+function usdtobgn (input) {
 
+    let usdAmount = Number(input[0]);
+    let usdPrice = 1.79549;
+    console.log(usdAmount * usdPrice);
 
 }
+
+usdtobgn ([12.5]);
