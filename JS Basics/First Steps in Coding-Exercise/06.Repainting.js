79ac42303c32997314f853ee69124090fb460f80
op46s <operator46@icloud.com>
2023-01-04 21:43:00 +0000

    21:42

---
+++ JS Basics/First Steps in Coding-Exercise/06.Repainting.js
@@ -0,0 +1,10 @@
+function repainting (input) {
+
+    let nylon = Number(input([0])) * 1.5;
+    let paint = Number(input([1])) * 14.5;
+    let thinner = Number(input([2])) * 5.0;
+    let bag = 0.4;
+
+        console.log (paint * 1.1);
+
+}
