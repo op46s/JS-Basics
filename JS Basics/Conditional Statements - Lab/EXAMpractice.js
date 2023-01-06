dad56a8e260e8587f4403ce11ca1ab0fdc5b872a
op46s <operator46@icloud.com>
2023-01-06 09:51:24 +0000

    09:51

--- JS Basics/Conditional Statements - Lab/EXAMpractice.js
+++ JS Basics/Conditional Statements - Lab/EXAMpractice.js
@@ -1,6 +1,6 @@
 // Exam Practice Tasks //
 //  ................. 01 ....................// 
-
+/*
  function oscars (input) {
 
         let rentCost = Number(input[0]);
@@ -13,5 +13,30 @@
             console.log(result); 
 }
 oscars (["5555"]); 
-
+*/
 //  ................. 02 ....................// 
+
+function programingFun(input)  {
+
+    let screeningTime = Number(input[0]);
+    let framesAmount = Number(input[1]);
+    let singleFrameTime = Number(input[2]);
+
+    let preparation = screeningTime * 0.15;
+    let totalMovieTime = (framesAmount * singleFrameTime) + preparation; 
+
+    let timeDifference = totalMovieTime - screeningTime;
+     console.log (totalMovieTime);
+     console.log (screeningTime);
+       
+    if (totalMovieTime <= screeningTime) {
+            console.log ("You managed to finish the movie on time! You have " + timeDifference + " minutes left!"); 
+        }
+
+        else {
+            console.log ("Time is up! To complete the movie you need " + timeDifference +" minutes."); 
+        }
+
+}
+
+programingFun (["60", "15", "3"]);
