793beaa160ac47b134e5fe31ad350a753d03bae4
op46s <operator46@icloud.com>
2023-01-24 13:22:37 +0000

    Office

--- JS Basics/Exams Practices/6-7 July 2019/05. Football Tournament.js
+++ JS Basics/Exams Practices/6-7 July 2019/05. Football Tournament.js
@@ -11,3 +11,13 @@ function footballTournament (input) {
 footballTournament(["Liverpool", "10", "W", "D", "D", "W", "L", "W", "D", "D", "W", "W"]);
 //footballTournament(["Barcelona", "7", "W", "D", "L", "L", "W", "W", "D"]);
 //footballTournament(["Chelsea", "0"]);	
+
+    let index = 0;
+    let teamName = input[index++];
+    let gamesPlayed = Number(input[index++]);
+    
+
+        for (i = 0; i < gamesPlayed; i++) {
+            let currentMatch = input[index++];
+            console.log(currentMatch);
+        }
