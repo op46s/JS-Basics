af8ac27f1b58382978b0cd29375c2f128ee6e456
op46s <operator46@icloud.com>
2023-01-27 11:17:08 +0000

    Office

---
+++ JS Basics/Exams Practices/6-7 April 2019/04.Cinema Voucher.js
@@ -0,0 +1,42 @@
+function cinemaVoucher (input) {
+
+    let ticket = 0;
+    let others = 0;
+
+    let index = 0;
+    let budget = Number(input[index]);
+    index++;
+    let wordS = input[index];
+    index++;
+
+    while (wordS !== 'End') {
+        let price = wordS.charCodeAt(0);
+        let isMovie = wordS.length > 8;
+    
+    if (isMovie) {
+        price += wordS.charCodeAt(1);
+    }
+
+    if (price > budget) {
+        break;
+        }
+
+    if (isMovie) {
+        ticket++;
+    } else {
+        others++;
+    }
+
+    budget -= price;
+
+    wordS = input[index];
+    index++;
+
+    }
+    console.log(`${ticket}`);
+    console.log(`${others}`);
+       
+}
+//cinemaVoucher(["300", "Captain Marvel", "popcorn", "Pepsi"]);
+cinemaVoucher(["1500", "Avengers: Endgame", "Bohemian Rhapsody", "Deadpool 2", "End"]);
+
