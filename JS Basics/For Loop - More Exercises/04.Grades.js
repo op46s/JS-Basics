383f96b9d9b998eab643a6bffab459d26a6f3366
op46s <operator46@icloud.com>
2023-01-28 16:18:10 +0000

    change grades input

--- JS Basics/For Loop - More Exercises/04.Grades.js
+++ JS Basics/For Loop - More Exercises/04.Grades.js
@@ -29,5 +29,5 @@ function grades (input) {
         console.log(`Fail: ${(fail / studentsCount * 100).toFixed(2)}%`);
         console.log(`Average: ${(totalAverage / studentsCount).toFixed(2)}`);
 }
-grades(["10", "3.00", "2.99", "5.68", "3.01", "4", "4", "6.00", "4.50", "2.44", "5"]);
-//grades(["6", "2", "3", "4", "5", "6", "2.2"]);
+// grades(["10", "3.00", "2.99", "5.68", "3.01", "4", "4", "6.00", "4.50", "2.44", "5"]);
+grades(["6", "2", "3", "4", "5", "6", "2.2"]);
