591c66336e0112de02b832f26811856c22851356
op46s <operator46@icloud.com>
2023-01-28 15:26:44 +0000

    add extra console log

--- JS Basics/For Loop - More Exercises/04.Grades.js
+++ JS Basics/For Loop - More Exercises/04.Grades.js
@@ -27,6 +27,7 @@ function grades (input) {
         console.log(`Between 4.00 and 4.99: ${(between45 / studentsCount * 100).toFixed(2)}%`);
         console.log(`Between 3.00 and 3.99: ${(between34 / studentsCount * 100).toFixed(2)}%`);
         console.log(`Fail: ${(fail / studentsCount * 100).toFixed(2)}%`);
+        console.log(`Fail: ${(fail / studentsCount * 100).toFixed(2)}%`);
         console.log(`Average: ${(totalAverage / studentsCount).toFixed(2)}`);
 }
 grades(["10", "3.00", "2.99", "5.68", "3.01", "4", "4", "6.00", "4.50", "2.44", "5"]);
