4022260bc808c5e048ac84195c21aba03445d313
op46s <operator46@icloud.com>
2023-02-05 18:34:36 +0000

    Home

--- JS Basics/For Loop - More Exercises/08.Equal Pairs.js
+++ JS Basics/For Loop - More Exercises/08.Equal Pairs.js
@@ -1,37 +1,49 @@
 function equalPairs (input) {
 
-    let index = 0;
-    let totalNumbers = Number(input[index++]);
+ let index = 0;
+ let totalPairs = Number(input[index]) * 2;
 
-    let number1 = 0;
-    let number2 = 0;
-    let result = 0;
-    let minValue = 0;
-    let maxValue = 0;
+ let myNumberOne = 0;
+ let myNumberTwo = 0;
 
+ let myNumberThree = 0;
+ let myNumberFour = 0;
 
-
-
-
-    for (let i = 0; i < totalNumbers; i++) {
-        number1 = Number(input[index++]);
-        number2 = Number(input[index++]);
+ let currentScore = 0;
+ let maxNumber = 0;
+ let maxDiff = 0;
+ 
+ for (let i = 1; i <= totalPairs; i++){
        
-        if (i === (totalNumbers - 1))
+    if (i % 2 != 0) {
+        myNumberOne = Number(input[i]);
+        console.log(myNumberOne);
+    } else {
+        myNumberTwo = Number(input[i]);
+        console.log(myNumberTwo);
+        currentScore = myNumberOne + myNumberTwo;
+    if (i < 3) {
+        continue;
+    }
+        maxDiff = currentScore;
+        if (maxDiff)
+
+
+
+        if (currentScore == currentScore) {
+            maxNumber = currentScore;
+        } else if (currentScore != currentScore) {
+            maxDiff = currentScore;
+            console.log("Diffe 1")
+        }
     }
 
-  
+ }
+ console.log(`Yes, value=${maxNumber}`)
     
-    
-    
-
-  
-//  console.log(sumOfTwo);
-//  console.log(sumOfTwo2);
-//  console.log(sumOfTwo3);
-
-
 }
-equalPairs(["3", "1", "2", "0", "3", "4", "-1"]);
+//equalPairs(["3", "1", "2", "0", "3", "4", "-1"]);
 //equalPairs(["4", "1", "1", "3", "1", "2", "2", "0", "0"]);
 //equalPairs(["2", "-1", "0", "0", "-1"]);
+//equalPairs(["1", "5", "5"])
+equalPairs(["2", "1", "2", "2", "2"]);
