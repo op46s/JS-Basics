b6d2faa422aeecb7262c73d566d67321e5d273a2
op46s <operator46@icloud.com>
2023-02-10 18:57:32 +0000

    Home

---
+++ JS Basics/Nested Loops - Lab/01.Clock.js
@@ -0,0 +1,12 @@
+function clock() {
+
+    for (let hours = 0; hours <=23; hours++) {
+        for (let minutes = 0; minutes <=59; minutes++) {
+            console.log(`${hours}:${minutes}`);
+        }
+    }
+}
+clock();
+
+// Test 123 
+// Test 345
