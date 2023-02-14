37484e2d4c2a64fd41a61faf68b6e0d0923fe1a7
op46s <operator46@icloud.com>
2023-02-14 06:52:23 +0000

    Home

--- JS Basics/Nested Loops - Lab/05.Traveling.js
+++ JS Basics/Nested Loops - Lab/05.Traveling.js
@@ -12,7 +12,7 @@ function traveling(input) {
     while (destination !== "End") {
         index++;
 
-        while (currentSavings <= budget) {
+        while (currentSavings < budget) {
             currentAmount = Number(input[index]);
             currentSavings += currentAmount;
             index++;  
@@ -26,42 +26,42 @@ function traveling(input) {
         
     }
 }
-traveling(["Greece",
-"1000",
-"200",
-"200",
-"300",
-"100",
-"150",
-"240",
-"Spain",
-"1200",
-"300",
-"500",
-"193",
-"423",
-"End"]);
-
-// traveling(["France",
-// "2000",
-// "300",
-// "300",
-// "200",
-// "400",
-// "190",
-// "258",
-// "360",
-// "Portugal",
-// "1450",
-// "400",
-// "400",
-// "200",
-// "300",
-// "300",
-// "Egypt",
-// "1900",
+// traveling(["Greece",
 // "1000",
-// "280",
+// "200",
+// "200",
+// "300",
+// "100",
+// "150",
+// "240",
+// "Spain",
+// "1200",
 // "300",
 // "500",
+// "193",
+// "423",
 // "End"]);
+
+traveling(["France",
+"2000",
+"300",
+"300",
+"200",
+"400",
+"190",
+"258",
+"360",
+"Portugal",
+"1450",
+"400",
+"400",
+"200",
+"300",
+"300",
+"Egypt",
+"1900",
+"1000",
+"280",
+"300",
+"500",
+"End"]);
