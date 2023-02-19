50f29374bbe3c79cc66a4737024ca3b1579f5f74
op46s <operator46@icloud.com>
2023-02-19 10:31:13 +0000

    Home

--- JS Basics/zNOTES/02. VSC Shortcuts.js
+++ JS Basics/zNOTES/02. VSC Shortcuts.js
@@ -1,3 +1,15 @@
 // CTRL + SHIFT + L --- Select all words and replace 
 //
 // Copy line above SHIFT + ALT + Up/Down.
+
+
+
+// function test(input) {
+
+// let myNum = Number(input[0]);
+
+// console.log(myNum % 100)
+
+
+// }
+// test(["123456"])
